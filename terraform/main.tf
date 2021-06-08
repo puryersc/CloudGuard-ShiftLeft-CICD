3405ad4be457f9d1e619c202980e4661f21c11a2
puryersc <60222448+puryersc@users.noreply.github.com>
2021-06-08 11:41:52 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,9 +32,9 @@ resource "aws_security_group" "prov_fw" {
   name = "prov_fw"
 
   ingress {
-    from_port = 22
+    from_port = 2222
     protocol = "tcp"
-    to_port = 22
+    to_port = 2222
     cidr_blocks = ["0.0.0.0/0"]
   }
 
